96094eddf62d50418f928858aa4031fd525e3c1e
WHB <60786437+Joma-Berg@users.noreply.github.com>
2020-02-09 23:22:53 +0800

    Add files via upload

---
+++ P5.py
@@ -0,0 +1,14 @@
+print('--------------王浩波制作----------')
+temp = input("猜一下我现在心里面想的是什么数字")
+guess = int(temp)
+if guess == 8:
+    print("哦豁，你是我心里的蛔虫吗?!")
+print("哼，猜中了也没有奖励")
+else:
+    if guess>8:
+        print('大了大了')
+        else:
+            print('小了小了')
+print("游戏结束，不玩啦^_^")
+        
+ 

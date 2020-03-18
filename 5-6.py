84fc34edfca3badd685b0ac3af67f323d36e34b8
WHB <60786437+Joma-Berg@users.noreply.github.com>
2020-03-19 01:06:18 +0800

    Add files via upload

---
+++ 5-6.py
@@ -0,0 +1,14 @@
+age=22
+if age<2:
+    print('baby')
+elif age>=2 and age<4:
+    print('learning walking')
+elif age>=4 and age<13:
+    print('child')
+elif age>=13 and age<20:
+    print('teeneger')
+elif age>=20 and age<65:
+    print('adult')
+else:
+    print('old man')
+    
